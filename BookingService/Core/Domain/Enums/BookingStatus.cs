fc3687666676ab88c294e505164a148012c15c55
Luiz Eugenio Barbieri <luizeugeniobarbieri@gmail.com>
2022-09-05 21:37:47 -0300

    Estruturação da camada de domínio.

---
+++ BookingService/Core/Domain/Enums/BookingStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enums
+{
+    public enum BookingStatus
+    {
+        Created = 1,
+        Paid = 2,
+        Finished = 3,
+        Canceled = 4,
+        Refounded = 5
+    }
+}
